5ca5f8cb561edf6e0f1c1c01d7d9d8ab50b24e29
Kleber <k.augsto@gmail.com>
2023-02-12 17:04:21 -0300

    início HTML e CSS

---
+++ js/imagens.js
@@ -0,0 +1,16 @@
+const fotos = [{
+    id:"1",
+    images: <img src="images/image0.jpeg"></img>
+},
+{
+    id:"2",
+    images: <img src="images/image1.jpeg"></img>
+},
+{
+    id:"3",
+    images: <img src="images/image2.jpeg"></img>
+},
+{
+    id:"4",
+    images: <img src="images/IMG_2838.jpg"></img>
+}]
---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+function carregarDados(){
+    console.log("Ta difícil")
+    }
+    
+var img01 
+var img02 
+var img03 
+var img04 
+
+img01 = <img src="image0.jpeg"></img>
+img02 = <img src="image1.jpeg"></img>
+img03 = <img src="image2.jpeg"></img>
+img04 = <img src="IMG_2838.jpeg"></img>
+
+
+function exibirDados(){
+
+}
